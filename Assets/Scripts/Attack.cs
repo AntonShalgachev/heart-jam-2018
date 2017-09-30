b2ec9e13cfdcd8769fc08f2a6d26242edb14c52a
Anton Shalgachev <anton.shalgachev@gmail.com>
2017-09-30 18:26:02 +0300

    health and attack systems

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+	public float damage;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		DealDamage(collision.gameObject);
+	}
+
+	void DealDamage(GameObject obj)
+	{
+		Debug.Log(string.Format("Trying to deal {0} damage to '{1}'", damage, obj.name));
+
+		var health = obj.GetComponent<Health>();
+		if (health)
+			health.TakeDamage(damage);
+	}
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+	public float maxHealth;
+
+	float health;
+
+	private void Start()
+	{
+		health = maxHealth;
+	}
+
+	public void TakeDamage(float damage)
+	{
+		Debug.Log(string.Format("Taking {0} damage", damage));
+		health -= damage;
+
+		if (health < 0.0f)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
