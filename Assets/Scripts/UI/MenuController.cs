787f41877ce9014c466ee699aeb084b5843f992a
Anton Shalgachev <anton.shalgachev@gmail.com>
2017-10-02 00:40:27 +0300

    main menu

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+	public Text tutorialText;
+	public Animation buttonAnimation;
+	public TutorialString[] tutorialStrings;
+
+	[Serializable]
+	public struct TutorialString
+	{
+		public string text;
+		public float delay;
+	}
+
+	private void Awake()
+	{
+		buttonAnimation.gameObject.SetActive(true);
+		tutorialText.gameObject.SetActive(false);
+	}
+
+	public void OnGameStart()
+	{
+		buttonAnimation.gameObject.SetActive(false);
+		tutorialText.gameObject.SetActive(true);
+
+		StartCoroutine(StartTutorial());
+	}
+
+	IEnumerator StartTutorial()
+	{
+		foreach (var data in tutorialStrings)
+		{
+			tutorialText.text = data.text;
+			yield return new WaitForSeconds(data.delay);
+		}
+
+		StartGame();
+	}
+
+	void StartGame()
+	{
+		SceneManager.LoadScene("Game");
+	}
+}
