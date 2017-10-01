15a511aaecd1be0928787de72149ec71a8821704
Anton Shalgachev <anton.shalgachev@gmail.com>
2017-10-01 09:40:09 +0300

    movement orientation

---
+++ Assets/Scripts/MovementOrientation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementOrientation : MonoBehaviour
+{
+	private Rigidbody2D body;
+
+	private void Awake()
+	{
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		if (body.velocity.magnitude > Mathf.Epsilon)
+			transform.rotation = Quaternion.FromToRotation(Vector2.right, body.velocity);
+	}
+}
