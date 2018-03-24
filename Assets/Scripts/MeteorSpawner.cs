3c905e922ceaa7fb45db50839153314d8124a26b
Anton Shalgachev <anton.shalgachev@gmail.com>
2018-03-24 03:33:46 +0300

    implement spawning and moving systems for mine meteors

---
+++ Assets/Scripts/MeteorSpawner.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSpawner : MonoBehaviour
+{
+    [Serializable]
+    public class MineMeteorPathParams
+    {
+        public List<GameObject> spawnPoints;
+        public List<GameObject> holdPoints;
+        public List<GameObject> destinationPoints;
+
+        public RandomHelper.Range speed;
+        public RandomHelper.Range holdDuration;
+    }
+
+    [SerializeField]
+    private List<MineMeteorPathParams> mineMeteorPaths;
+    [SerializeField]
+    private float mineMeteorSpawnPeriod;
+
+    [SerializeField]
+    private GameObject minerMeteorPrefab;
+
+    private GameObject meteorHolder;
+
+    private void Start()
+    {
+        meteorHolder = new GameObject("Meteors");
+
+        StartCoroutine(StartSpawningMineMeteors());
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private MineMeteorMovement.Path getRandomMineMeteorPath()
+    {
+        Debug.Assert(mineMeteorPaths.Count > 0);
+
+        var random = RandomHelper.Instance();
+        var pathParams = random.GetItem(mineMeteorPaths);
+
+        var path = new MineMeteorMovement.Path();
+        path.spawnPoint = random.GetItem(pathParams.spawnPoints);
+        path.holdPoint = random.GetItem(pathParams.holdPoints);
+        path.destinationPoint = random.GetItem(pathParams.destinationPoints);
+
+        path.speed = pathParams.speed.GetRandom();
+        path.holdDuration = pathParams.holdDuration.GetRandom();
+
+        return path;
+    }
+
+    private void SpawnMineMeteor(MineMeteorMovement.Path path)
+    {
+        var meteor = Instantiate(minerMeteorPrefab, path.spawnPoint.transform.position, Quaternion.identity, meteorHolder.transform);
+        var meteorMovement = meteor.GetComponent<MineMeteorMovement>();
+        Debug.Assert(meteorMovement);
+
+        meteorMovement.SetPath(path);
+    }
+
+    private IEnumerator StartSpawningMineMeteors()
+    {
+        while (true)
+        {
+            var path = getRandomMineMeteorPath();
+            SpawnMineMeteor(path);
+            yield return new WaitForSeconds(mineMeteorSpawnPeriod);
+        }
+    }
+}
---
+++ Assets/Scripts/MineMeteorMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineMeteorMovement : MonoBehaviour
+{
+    public class Path
+    {
+        public GameObject spawnPoint;
+        public GameObject holdPoint;
+        public GameObject destinationPoint;
+
+        public float speed;
+        public float holdDuration;
+    }
+
+    private enum State
+    {
+        MovingIn,
+        Holding,
+        MovingOut,
+    }
+
+    Path path;
+
+    Vector3 target;
+    float delay;
+    State state;
+
+    private void Update()
+    {
+        if (path == null)
+            return;
+
+        delay -= Time.deltaTime;
+
+        if (delay < 0.0f)
+        {
+            // adds some sort of easing
+            var speed = path.speed * DistanceToTarget();
+
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+            const float Epsilon = 0.1f;
+
+            if (DistanceToTarget() < Epsilon)
+                nextState();
+        }
+    }
+
+    private float DistanceToTarget()
+    {
+        return (transform.position - target).magnitude;
+    }
+
+    public void SetPath(Path path)
+    {
+        this.path = path;
+
+        target = path.holdPoint.transform.position;
+        state = State.MovingIn;
+    }
+
+    private void nextState()
+    {
+        switch(state)
+        {
+            case State.MovingIn:
+                state = State.Holding;
+                target = path.holdPoint.transform.position;
+                delay = path.holdDuration;
+                break;
+            case State.Holding:
+                state = State.MovingOut;
+                target = path.destinationPoint.transform.position;
+                delay = 0.0f;
+                break;
+            case State.MovingOut:
+                Destroy(gameObject);
+                break;
+        }
+    }
+}
